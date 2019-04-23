a584dea3bed14b52b7e89762e46e2b725385baed
Fredx <freddy87@gmail.com>
2019-04-23 19:53:29 +0200

    Added board logic

---
+++ src/board/board.test.ts
@@ -0,0 +1,190 @@
+import { some } from "fp-ts/lib/Option";
+import {
+  Board,
+  boardCell,
+  BoardCell,
+  cellHasMine,
+  countAllAdjacentMines,
+  createBoard,
+  createEmptyBoard,
+  getAdjacentCells,
+  getBoardCellIndex,
+  isCellFlagged,
+  isCellRevealed,
+  placeMineInCell,
+  setCellFlagged,
+  setCellRevealed
+} from "./board";
+
+describe("create board", () => {
+  test("cannot create board with more mines then cells", () => {
+    expect(
+      createBoard(10, 5, 200)
+        .run()
+        .isLeft()
+    ).toBe(true);
+  });
+
+  test("successfully create an empty board with specified rows and columns", () => {
+    const board = createEmptyBoard(5, 10);
+    expect(board.cols).toBe(10);
+    expect(board.rows).toBe(5);
+    expect(board.minesIndexes.size).toBe(0);
+    expect(board.flaggedIndexes.size).toBe(0);
+    expect(board.cellRevealStatus.length).toBe(50);
+  });
+
+  test("successfully create board with specified number of mines", () => {
+    const eitherBoard = createBoard(10, 5, 20).run();
+    expect(eitherBoard.isRight()).toBe(true);
+
+    const board = eitherBoard.value as Board;
+    expect(board.rows).toBe(10);
+    expect(board.cols).toBe(5);
+    expect(board.minesIndexes.size).toBe(20);
+    expect(board.flaggedIndexes.size).toBe(0);
+    expect(board.cellRevealStatus.length).toBe(50);
+  });
+});
+
+describe("board functions", () => {
+  let board: Board;
+
+  beforeEach(() => {
+    board = createEmptyBoard(10, 15) as Board;
+  });
+
+  test("getBoardCellIndex returns none when row less than 0", () => {
+    expect(getBoardCellIndex(boardCell(-1, 10), board).isNone()).toBe(true);
+  });
+
+  test("getBoardCellIndex returns none when col less than 0", () => {
+    expect(getBoardCellIndex(boardCell(7, -1), board).isNone()).toBe(true);
+  });
+
+  test("getBoardCellIndex returns none when row greter than or equal rows", () => {
+    expect(getBoardCellIndex(boardCell(10, 10), board).isNone()).toBe(true);
+    expect(getBoardCellIndex(boardCell(100, 10), board).isNone()).toBe(true);
+  });
+
+  test("getBoardCellIndex returns none when col greter than or equal cols", () => {
+    expect(getBoardCellIndex(boardCell(7, 15), board).isNone()).toBe(true);
+    expect(getBoardCellIndex(boardCell(7, 100), board).isNone()).toBe(true);
+  });
+
+  test.each([
+    [0, 0, 0],
+    [0, 2, 2],
+    [0, 14, 14],
+    [1, 0, 15],
+    [1, 14, 29],
+    [2, 0, 30],
+    [9, 14, 149]
+  ])(
+    "getBoardCellIndex returns the right index when row=%i and col=%i",
+    (row, col, expexted) => {
+      expect(getBoardCellIndex(boardCell(row, col), board)).toEqual(
+        some(expexted)
+      );
+    }
+  );
+
+  test("cellHasMine / placeMineInCell", () => {
+    expect(cellHasMine(boardCell(2, 4), board)).toEqual(some(false));
+    placeMineInCell(boardCell(2, 4), board).map(newBoard => {
+      expect(cellHasMine(boardCell(2, 4), newBoard)).toEqual(some(true));
+    });
+  });
+
+  test("isCellFlagged / setCellFlagged", () => {
+    expect(isCellFlagged(boardCell(4, 6), board)).toEqual(some(false));
+    setCellFlagged(boardCell(4, 6), board).map(newBoard => {
+      expect(isCellFlagged(boardCell(4, 6), newBoard)).toEqual(some(true));
+    });
+  });
+
+  test("isCellRevealed / setCellRevealed", () => {
+    expect(isCellRevealed(boardCell(7, 4), board)).toEqual(some(false));
+    setCellRevealed(boardCell(7, 4), board).map(newBoard => {
+      expect(isCellRevealed(boardCell(7, 4), newBoard)).toEqual(some(true));
+    });
+  });
+
+  test.each<[number, number, BoardCell[]]>([
+    [-1, -1, []],
+    [0, 0, [boardCell(0, 1), boardCell(1, 0), boardCell(1, 1)]],
+    [
+      5,
+      0,
+      [
+        boardCell(4, 0),
+        boardCell(4, 1),
+        boardCell(5, 1),
+        boardCell(6, 1),
+        boardCell(6, 0)
+      ]
+    ],
+    [
+      5,
+      5,
+      [
+        boardCell(5, 4),
+        boardCell(4, 4),
+        boardCell(4, 5),
+        boardCell(4, 6),
+        boardCell(5, 6),
+        boardCell(6, 6),
+        boardCell(6, 5),
+        boardCell(6, 4)
+      ]
+    ],
+    [
+      9,
+      9,
+      [
+        boardCell(9, 8),
+        boardCell(8, 8),
+        boardCell(8, 9),
+        boardCell(8, 10),
+        boardCell(9, 10)
+      ]
+    ],
+    [9, 14, [boardCell(9, 13), boardCell(8, 13), boardCell(8, 14)]],
+    [10, 15, []]
+  ])(
+    "getAdjacentCells for row %i and col %i",
+    (row: number, col: number, expexted: BoardCell[]) => {
+      const result = getAdjacentCells(boardCell(row, col), board);
+      expect(result).toIncludeSameMembers(expexted);
+    }
+  );
+
+  test("countAllAdjacentMines", () => {
+    placeMineInCell(boardCell(2, 2), board)
+      .chain(b => placeMineInCell(boardCell(5, 5), b))
+      .chain(b => placeMineInCell(boardCell(5, 6), b))
+      .chain(b => placeMineInCell(boardCell(6, 6), b))
+      .map(b => countAllAdjacentMines(b))
+      .map(b => {
+        expect(b.minesIndexes.size).toBe(4);
+        getBoardCellIndex(boardCell(0, 0), b).map(i => {
+          expect(b.adjacentMinesCount[i]).toBe(0);
+        });
+        getBoardCellIndex(boardCell(1, 1), b).map(i => {
+          expect(b.adjacentMinesCount[i]).toBe(1);
+        });
+        getBoardCellIndex(boardCell(2, 1), b).map(i => {
+          expect(b.adjacentMinesCount[i]).toBe(1);
+        });
+        getBoardCellIndex(boardCell(2, 2), b).map(i => {
+          expect(b.adjacentMinesCount[i]).toBe(0);
+        });
+        getBoardCellIndex(boardCell(6, 5), b).map(i => {
+          expect(b.adjacentMinesCount[i]).toBe(3);
+        });
+        getBoardCellIndex(boardCell(6, 7), b).map(i => {
+          expect(b.adjacentMinesCount[i]).toBe(2);
+        });
+      });
+  });
+});
---
+++ src/board/board.ts
@@ -0,0 +1,201 @@
+import { makeBy, updateAt } from "fp-ts/lib/Array";
+import { Either, left, right } from "fp-ts/lib/Either";
+import {
+  fromEither,
+  fromLeft,
+  IOEither,
+  ioEither,
+  right as rightIOE
+} from "fp-ts/lib/IOEither";
+import { none, Option, some } from "fp-ts/lib/Option";
+import { randomInt } from "fp-ts/lib/Random";
+
+export interface Board {
+  rows: number;
+  cols: number;
+  minesIndexes: Set<number>;
+  flaggedIndexes: Set<number>;
+  cellRevealStatus: boolean[];
+  adjacentMinesCount: number[];
+}
+
+export interface BoardCell {
+  row: number;
+  col: number;
+}
+
+export function boardCell(row: number, col: number): BoardCell {
+  return { row, col };
+}
+
+export function createEmptyBoard(rows: number, cols: number): Board {
+  return {
+    rows,
+    cols,
+    minesIndexes: new Set<number>(),
+    flaggedIndexes: new Set<number>(),
+    cellRevealStatus: makeBy(rows * cols, () => false),
+    adjacentMinesCount: makeBy(rows * cols, () => 0)
+  };
+}
+
+export function isValidBoardCell(cell: BoardCell, board: Board): boolean {
+  return (
+    cell.row >= 0 &&
+    cell.row < board.rows &&
+    cell.col >= 0 &&
+    cell.col < board.cols
+  );
+}
+
+export function getBoardCellIndex(
+  cell: BoardCell,
+  board: Board
+): Option<number> {
+  return isValidBoardCell(cell, board)
+    ? some(cell.row * board.cols + cell.col)
+    : none;
+}
+
+export function cellHasMine(cell: BoardCell, board: Board): Option<boolean> {
+  return getBoardCellIndex(cell, board).chain(i =>
+    some(board.minesIndexes.has(i))
+  );
+}
+
+export function placeMineInCell(cell: BoardCell, board: Board): Option<Board> {
+  return getBoardCellIndex(cell, board).chain(i => placeMineInIndex(i, board));
+}
+
+export function isCellFlagged(cell: BoardCell, board: Board): Option<boolean> {
+  return getBoardCellIndex(cell, board).chain(i =>
+    some(board.flaggedIndexes.has(i))
+  );
+}
+
+export function setCellFlagged(cell: BoardCell, board: Board): Option<Board> {
+  return getBoardCellIndex(cell, board).chain(i => {
+    const flaggedIndexes = new Set(board.flaggedIndexes);
+    flaggedIndexes.add(i);
+    return some({ ...board, flaggedIndexes });
+  });
+}
+
+export function isCellRevealed(cell: BoardCell, board: Board): Option<boolean> {
+  return getBoardCellIndex(cell, board).chain(i =>
+    some(board.cellRevealStatus[i])
+  );
+}
+
+export function setCellRevealed(cell: BoardCell, board: Board): Option<Board> {
+  return getBoardCellIndex(cell, board)
+    .chain(i => {
+      return updateAt(i, true, board.cellRevealStatus);
+    })
+    .chain(cellRevealStatus =>
+      some({
+        ...board,
+        cellRevealStatus
+      })
+    );
+}
+
+export function isCellEmpty(cell: BoardCell, board: Board): Option<boolean> {
+  return getBoardCellIndex(cell, board).chain(i =>
+    some(board.adjacentMinesCount[i] === 0)
+  );
+}
+
+export function getAdjacentCells(cell: BoardCell, board: Board): BoardCell[] {
+  if (!isValidBoardCell(cell, board)) {
+    return [];
+  }
+  const res = [];
+  for (let i = cell.row - 1; i < cell.row + 2; i++) {
+    for (let j = cell.col - 1; j < cell.col + 2; j++) {
+      const newCell = boardCell(i, j);
+      if (
+        (i !== cell.row || j !== cell.col) &&
+        isValidBoardCell(newCell, board)
+      ) {
+        res.push(newCell);
+      }
+    }
+  }
+  return res;
+}
+
+function placeMineInIndex(index: number, board: Board): Option<Board> {
+  if (index < 0 || index >= board.rows * board.cols) {
+    return none;
+  }
+  const minesIndexes = new Set(board.minesIndexes);
+  minesIndexes.add(index);
+  return some({ ...board, minesIndexes });
+}
+
+function tryPlaceMineInIndex(
+  index: number,
+  board: Board
+): Either<string, Board> {
+  return board.minesIndexes.has(index)
+    ? left("mine already present")
+    : placeMineInIndex(index, board).fold(left("wrong index"), b => right(b));
+}
+
+export function placeRandomMine(board: Board): IOEither<string, Board> {
+  return rightIOE<string, number>(
+    randomInt(0, board.rows * board.cols - 1)
+  ).chain(i => fromEither(tryPlaceMineInIndex(i, board)));
+}
+
+function placeNextMine(board: Board): IOEither<never, Board> {
+  return placeRandomMine(board)
+    .orElse(() => placeNextMine(board))
+    .chain(newBoard => ioEither.of(newBoard));
+}
+
+function placeMinesInBoard(
+  board: Board,
+  minesCount: number
+): IOEither<never, Board> {
+  return placeNextMine(board).chain(newBoard =>
+    newBoard.minesIndexes.size === minesCount
+      ? ioEither.of(newBoard)
+      : placeMinesInBoard(newBoard, minesCount)
+  );
+}
+
+export function countAdjacentMines(cell: BoardCell, board: Board): number {
+  const adjacentCells = getAdjacentCells(cell, board);
+  return adjacentCells.reduce(
+    (total, current) =>
+      cellHasMine(current, board).fold(total, v => (v ? total + 1 : total)),
+    0
+  );
+}
+
+export function countAllAdjacentMines(board: Board): Board {
+  const adjacentMinesCount: number[] = [];
+  for (let i = 0; i < board.rows; i++) {
+    for (let j = 0; j < board.cols; j++) {
+      const cell = boardCell(i, j);
+      getBoardCellIndex(cell, board).map(index => {
+        adjacentMinesCount[index] = countAdjacentMines(cell, board);
+      });
+    }
+  }
+  return { ...board, adjacentMinesCount };
+}
+
+export function createBoard(
+  rows: number,
+  cols: number,
+  mines: number
+): IOEither<string, Board> {
+  if (mines > cols * rows) {
+    return fromLeft("Cannot create a board with more mines than cells");
+  }
+  const board = createEmptyBoard(rows, cols);
+  return placeMinesInBoard(board, mines).map(countAllAdjacentMines);
+}
